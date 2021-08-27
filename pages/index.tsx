6d07c8f773c6907c840e5b79deeb1bfbc134f371
r3ticuli <messs@seas.upenn.edu>
2021-08-27 16:33:54 -0400

    added dummy data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,39 @@
-import { useEffect, useState } from 'react'
-import Web3 from 'web3'
-import Web3Modal from 'web3modal'
-import { attemptPurchase, initWeb3 } from '../lib/web3'
+import React, { useEffect, useState } from 'react'
+import { Gallery, GalleryTile } from '../components/gallery'
+import { initWeb3 } from '../lib/web3'
+
+const artist = {
+  userName: 'Van Gogh',
+  address: '0x5c611cb1afd116e0bf40aa00297688d2f8e6c9c2',
+  galleries: [
+    {
+      name: 'Blue Period',
+      address: '0x36Ff249bCa4B82e99c5227337AEb87B0591E160f',
+      images: ['./blue.jpeg'],
+    },
+  ],
+}
 
 export const Home = (): JSX.Element => {
-  const [web3Modal, setWeb3Modal] = useState<Web3Modal | undefined>(undefined)
-  const [web3Provider, setWeb3Provider] = useState<Web3 | undefined>(undefined)
+  const [api, setApi] = useState(undefined)
 
   useEffect(() => {
-    if (!web3Provider) {
-      initWeb3(setWeb3Provider, setWeb3Modal)
+    if (!api) {
+      initWeb3(setApi)
     }
   }, [])
 
-  if (!web3Provider) return <h1> loading </h1>
+  if (!api) return <h1> loading </h1>
 
   return (
     <main>
       <h1 className="title">Web3Modal Example</h1>
-      {web3Modal && (
-        <button
-          className="button"
-          type="button"
-          onClick={async () => await attemptPurchase(web3Modal, web3Provider)}
-        >
-          Send Funds
-        </button>
+      {api && (
+        <>
+          {artist.galleries.map((gallery: Gallery) => (
+            <GalleryTile key={gallery.address} gallery={gallery} api={api} />
+          ))}
+        </>
       )}
     </main>
   )
