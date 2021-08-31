b3d5dd7150a7124427d3cc13fd7e1b503b9be647
r3ticuli <messs@seas.upenn.edu>
2021-08-31 15:55:56 -0400

    slick-carousel css global style

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import 'slick-carousel/slick/slick-theme.css'
+import 'slick-carousel/slick/slick.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
