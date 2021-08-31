f2f1299d0b6631d5bcbc16e10d0399c245e36345
r3ticuli <messs@seas.upenn.edu>
2021-08-31 16:08:47 -0400

    add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import 'slick-carousel/slick/slick-theme.css'
 import 'slick-carousel/slick/slick.css'
+import 'tailwindcss/tailwind.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
