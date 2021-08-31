975e5d8e738e7edb516ca0b5895c53b90e1bd2ef
r3ticuli <messs@seas.upenn.edu>
2021-08-31 15:55:10 -0400

    clean up gallery style

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import Slider from 'react-slick'
 import { MediciAPI } from '../lib/web3'
 
 export interface Artist {
@@ -12,6 +13,14 @@ export interface Gallery {
   address: string
   images: string[]
   price: string
+}
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
 }
 
 export const GalleryTile = ({
@@ -34,31 +43,46 @@ export const GalleryTile = ({
   }, [])
 
   return (
-    <div>
+    <div className="galleryTile">
       <h3>{name}</h3>
-      {images.map((i: string) => (
-        <img
-          onContextMenu={(e) => e.preventDefault()}
-          key={i}
-          src={hasPaid ? i : './gallery_placeholder.png'}
-        ></img>
-      ))}
-      <style jsx>{`
-        img {
-          max-width: 200px;
-        }
-      `}</style>
-      {hasPaid || (
-        <button
-          className="button"
-          type="button"
-          onClick={async () => {
-            await api.attemptPurchase(address)
-          }}
-        >
-          Send Funds
-        </button>
+      {hasPaid ? (
+        <>
+          <Slider {...settings}>
+            {images.map((i: string) => (
+              <img
+                onContextMenu={(e) => e.preventDefault()}
+                key={i}
+                src={i}
+              ></img>
+            ))}
+          </Slider>
+        </>
+      ) : (
+        <>
+          <img src={'./gallery_placeholder.png'}></img>
+          <button
+            className="button"
+            type="button"
+            onClick={async () => {
+              await api.attemptPurchase(address)
+            }}
+          >
+            Send Funds
+          </button>
+        </>
       )}
+      <style global jsx>
+        {`
+          .galleryTile {
+            max-width: 400px;
+            margin: 0 auto;
+          }
+          .slick-prev:before,
+          .slick-next:before {
+            color: red;
+          }
+        `}
+      </style>
     </div>
   )
 }
