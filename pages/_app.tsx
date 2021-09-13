59462d2251620aa04b74b203232e0146b2afa91d
sbmsr <sebastian.messier@gmail.com>
2021-09-13 16:30:07 -0400

    init auth (github)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import { SessionProvider } from 'next-auth/react'
+import React from 'react'
 import 'slick-carousel/slick/slick-theme.css'
 import 'slick-carousel/slick/slick.css'
 import 'tailwindcss/tailwind.css'
 
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
---
+++ pages/admin.tsx
@@ -0,0 +1,43 @@
+import { getSession, signIn, signOut, useSession } from 'next-auth/react'
+import React, { useEffect, useState } from 'react'
+import { initWeb3 } from '../lib/web3'
+
+export const Admin = (): JSX.Element => {
+  const { data: session } = useSession()
+
+  const [api, setApi] = useState(undefined)
+
+  useEffect(() => {
+    if (!api) {
+      initWeb3(setApi)
+    }
+  }, [])
+
+  if (!api) return <h1> loading </h1>
+
+  return (
+    <main className="flex flex-col items-center m-auto">
+      {session ? (
+        <>
+          Signed in as {session.user.name} <br />
+          <button onClick={() => signOut()}>Sign out</button>{' '}
+        </>
+      ) : (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+    </main>
+  )
+}
+
+export async function getServerSideProps(ctx: any): Promise<any> {
+  return {
+    props: {
+      session: await getSession(ctx),
+    },
+  }
+}
+
+export default Admin
