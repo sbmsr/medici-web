c6d21871758c7b341b41c7e534ac40a3b30b7977
sbmsr <sebastian.messier@gmail.com>
2021-09-16 22:41:05 -0400

    db schema

--- pages/api/_db.ts
+++ pages/api/_db.ts
@@ -12,9 +12,9 @@ export interface User {
 }
 
 db.define(
-  'User',
+  'users',
   {
-    publicAddress: {
+    address: {
       allowNull: false,
       type: DataTypes.STRING,
       unique: true,
@@ -29,6 +29,126 @@ db.define(
     username: {
       type: DataTypes.STRING,
       unique: true,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+)
+
+db.define(
+  'roles',
+  {
+    id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+    },
+  },
+  {
+    timestamps: false,
+  }
+)
+
+db.define(
+  'user_roles',
+  {
+    address: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      references: {
+        model: 'users',
+        key: 'address',
+      },
+    },
+    role: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      references: {
+        model: 'roles',
+        key: 'id',
+      },
+    },
+  },
+  {
+    timestamps: false,
+  }
+)
+
+db.define(
+  'stores',
+  {
+    id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    contractAddress: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    owner: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'address',
+      },
+    },
+  },
+  {
+    timestamps: false,
+  }
+)
+
+db.define(
+  'images',
+  {
+    gallery: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'galleries',
+        key: 'id',
+      },
+      primaryKey: true,
+    },
+    url: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: false,
+  }
+)
+
+db.define(
+  'galleries',
+  {
+    store: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'stores',
+        key: 'id',
+      },
+      primaryKey: true,
+    },
+    id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
   },
   {
